31307e247d87a29503b2a53632f064fb5dbde0c4
CriistianEspinosa <espinosacristian508@gmail.com>
2024-06-18 11:01:11 -0500

    Java

---
+++ index.js
@@ -0,0 +1,13 @@
+function onClickSubmitButton(){
+let inputValue= Document.getElementById('InputText'). value;
+if (inputValue>0){
+alert('Hey el numero es mayor que cero')
+}
+if (inputValue<0){
+    alert('Hey el numero es menor que cero')
+    }
+    if (inputValue==0){
+        alert('Hey el numero es igual que cero')
+        }
+        
+}
